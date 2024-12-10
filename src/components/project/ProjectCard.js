09265aebb9de1fb8d5844a2939a055eb67cd0842
Rafaela Wolf <rafaelawolf52@gmail.com>
2024-12-10 11:10:14 -0300

    adicionando a função de remover projetos

--- src/components/project/ProjectCard.js
+++ src/components/project/ProjectCard.js
@@ -3,6 +3,12 @@ import styles from "./ProjectCard.module.css";
 import { BsPencil, BsFillTrashFill } from "react-icons/bs";
 
 function ProjectCard({ id, name, budget, category, handleRemove }) {
+
+    const remove = (e) => {
+        e.preventDefault();
+        handleRemove(id);
+    }
+
     return (
         <div className={styles.project_card}>
             <h4>{name}</h4>
@@ -13,7 +19,7 @@ function ProjectCard({ id, name, budget, category, handleRemove }) {
                 <Link to="">
                     <BsPencil>Edit</BsPencil>
                 </Link>
-                <button>
+                <button onClick={remove}>
                     <BsFillTrashFill>Remove</BsFillTrashFill>
                 </button>
             </div>

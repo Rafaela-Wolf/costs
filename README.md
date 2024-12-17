237789803cd738be1bd3b9681f4763bf77862558
Rafaela Wolf <rafaelawolf52@gmail.com>
2024-12-17 13:02:47 -0300

    adicionando o responsivo ao navbar

---
+++ README.md
@@ -0,0 +1,17 @@
+# 📊 Costs
+
+**Costs** é um aplicativo de gerenciamento de projetos onde os usuários podem adicionar, editar e excluir projetos, além de acompanhar os custos envolvidos. O projeto foi desenvolvido utilizando **React** como parte do meu aprendizado em desenvolvimento fullstack.
+
+## 🚀 **Funcionalidades**
+
+- **Adicionar projetos** com informações relevantes.
+- **Acompanhar custos** de cada projeto.
+- **Editar e excluir** projetos e serviços.
+- **Interatividade dinâmica** com formulários e manipulação de dados.
+
+## 🛠️ **Tecnologias Utilizadas**
+
+- **React**: Framework JavaScript para construção de interfaces.
+- **Vite**: Ferramenta para otimizar o desenvolvimento em React.
+- **CSS**: Estilização da aplicação.
+- **HTML**: Estrutura básica da aplicação.
--- src/components/layouts/Navbar.js
+++ src/components/layouts/Navbar.js
@@ -1,16 +1,33 @@
 import { Link } from "react-router-dom";
+import React, { useState } from "react";
+
 import Container from "./Container";
 import styles from "./Navbar.module.css";
 import logo from "../../assets/images/logo-costs.png";
 
 function Navbar() {
+
+    const [menuOpen, setMenuOpen] = useState(false); // Controle do menu
+
+    const toggleMenu = () => {
+        setMenuOpen(!menuOpen); // Altera o estado ao clicar no hamburger
+    };
+
     return (
         <nav className={styles.navbar}>
+
             <Container>
-            <Link to="/">
-                <img className={styles.logo} src={logo} alt="Logo Costs"></img>
-            </Link>
-                <ul className={styles.list}>
+                <Link to="/">
+                    <img className={styles.logo} src={logo} alt="Logo Costs"></img>
+                </Link>
+
+                <div className={styles.hamburger} onClick={toggleMenu}>
+                    <div></div>
+                    <div></div>
+                    <div></div>
+                </div>
+
+                <ul className={`${styles.list} ${menuOpen ? styles.active : ""}`}>
                     <li className={styles.item}><Link to="/">Home</Link></li>
                     <li className={styles.item}><Link to="/projects">Projects</Link></li>
                     <li className={styles.item}><Link to="/company">Company</Link></li>
